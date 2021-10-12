8e08e20ae8d6a7985e56fa0f944e6db84de0798f
Lucas <marte.lu@hotmail.com.br>
2021-10-12 17:38:41 -0300

    Cadastro de usuarios FireBase

--- app/src/main/java/com/marcos/projetofirebase/FormCadastro.java
+++ app/src/main/java/com/marcos/projetofirebase/FormCadastro.java
@@ -1,17 +1,123 @@
 package com.marcos.projetofirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 public class FormCadastro extends AppCompatActivity
 {
+
+
+    private EditText edit_nome,edit_email,edit_senha;
+    private Button bt_cadastrar;
+    String[] mensagens = {"Preencha todos os campos", "Cadastro realizado com sucesso"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_cadastro);
+
         getSupportActionBar().hide();
+        IniciarComponentes();
+
+        bt_cadastrar.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+                String nome = edit_nome.getText().toString();
+                String email = edit_email.getText().toString();
+                String senha = edit_senha.getText().toString();
+
+
+                if (nome.isEmpty() || email.isEmpty() || senha.isEmpty())
+                {
+                    Snackbar snackbar = Snackbar.make(v,mensagens[0],Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }
+                else{
+                    CadastrarUsuario(v);
+                }
+            }
+        });
+    }
+
+           private void CadastrarUsuario(View v){
+
+                String email = edit_email.getText().toString();
+                String senha = edit_senha.getText().toString();
+
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,senha).addOnCompleteListener(new OnCompleteListener<AuthResult>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                if (task.isSuccessful())
+                 {
+                   Snackbar snackbar = Snackbar.make(v,mensagens[1],Snackbar.LENGTH_SHORT);
+                   snackbar.setBackgroundTint(Color.WHITE);
+                   snackbar.setTextColor(Color.BLACK);
+                   snackbar.show();
+                 }
+                  else {
+                    String erro;
+                    try {
+                        throw task.getException();
+
+                        }
+                    catch (FirebaseAuthWeakPasswordException e)
+                    {
+                        erro = "Digite uma senha com no mínimo 6 caracteres";
+                    }
+
+                    catch (FirebaseAuthUserCollisionException e)
+                    {
+                        erro = "Está conta já esta cadastrada";
+                    }
+                    catch (FirebaseAuthInvalidCredentialsException e)
+                    {
+                        erro = "E-mail inválido";
+                    }
+                    catch (Exception e)
+                    {
+                        erro = "Erro ao cadastrar usuário";
+                    }
+
+                    Snackbar snackbar = Snackbar.make(v,erro,Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+
+
+                     }
+                   }
+                });
+
+           }
+
+    private void IniciarComponentes(){
+        edit_nome = findViewById(R.id.edit_nome);
+        edit_email = findViewById(R.id.edit_email);
+        edit_senha = findViewById(R.id.edit_senha);
+        bt_cadastrar = findViewById(R.id.bt_cadastrar);
     }
 }
