ca95d93344b5f90ed5948cd739f91316b6a69261
Lucas <marte.lu@hotmail.com.br>
2021-10-11 22:56:17 -0300

    telas login castro e menu

---
+++ app/src/main/java/com/marcos/projetofirebase/TelaPrincipal.java
@@ -0,0 +1,17 @@
+package com.marcos.projetofirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaPrincipal extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_principal);
+        getSupportActionBar().hide();
+    }
+}
